9929ef59f61c0c70a56b5b308e0382697fb2691e
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-03-06 18:23:31 +0000

    Added Notes on Hofs callback and closures with problems

---
+++ 01. Front-End/24. Callback and Closure in JS/01. Code/app.js
@@ -0,0 +1,38 @@
+// Q1. Create a function that takes another function as an arguments and calls it after 3 seconds (HOF + Callbacks)
+
+function hello(fn){
+    return function(){
+       setTimeout(()=>{
+        fn()
+       },3000)
+    }
+}
+
+// let f1 = hello(function(){
+//     console.log("Hello");
+// })
+// f1()
+
+function hello1(fn){
+    let time = 0
+    
+    return function(){
+        let newTime = new Date().getTime()
+        if(time <= newTime){
+            fn()
+            time = newTime + 3000    
+          }else{
+            console.log("Wait for some time to run again minimum waiting time 3 second");
+        }
+    }
+}
+
+let fn = hello1(function(){
+    console.log(`
+Function is runing.................!!
+        `)
+})
+
+setInterval(()=>{
+    fn()
+},1000)
