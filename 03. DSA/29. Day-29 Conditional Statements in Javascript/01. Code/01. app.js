84edd2cebbbe00dfa3ad53042ebfed410114d268
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-03-11 04:49:06 +0000

    Added detailed notes and explanations on JavaScript conditional statements, loops (entry & exit controlled), Math functions, leap year logic, shop discount calculation, electricity bill calculation, salary calculation, and movie rating system

--- 03. DSA/29. Day-29 Conditional Statements in Javascript/01. Code/01. app.js
+++ 03. DSA/29. Day-29 Conditional Statements in Javascript/01. Code/01. app.js
@@ -55,4 +55,41 @@ let isLeap = false
 // console.log(`You have to pay ${ammount - (discount * ammount)/100} after discount`);
 
 // Q3. Electry bill
+// Approach -1 
+// let unit = Number(prompt("Enter unit"))
+// let ammount = 0
 
+// Bottom to Top approach 
+// if(unit > 0 && unit <= 100){
+//     ammount = unit * 4.2
+// }else if(unit > 100 && unit <= 200){
+//     ammount = (100*4.2) + (unit-100)*6
+// }else if(unit > 200 && unit <= 400){
+//     ammount = (100*4.2) + (100*6) + (unit-200)*8
+// }else if(unit > 400){
+//     ammount = (100*4.2) + (100*6) + (200*8) + (unit-400) * 13
+// }
+// console.log(ammount);
+
+// Approach - 2 
+// Top to Bottom approach 
+// Using Multiple if 
+
+let unit = Number(prompt("Enter unit"))
+let ammount = 0
+
+if(unit > 400){
+    ammount = (unit - 400) * 13
+    unit = 400
+}
+if(unit > 200 && unit <= 400){
+    ammount = ammount + (unit - 200) * 8
+    unit = 200
+}
+if(unit > 100 && unit <= 200){
+    ammount = ammount + (unit - 100) * 6
+    unit = 100
+}
+ammount = ammount + (unit - 0) * 4.2
+
+console.log(ammount);
---
+++ 03. DSA/29. Day-29 Conditional Statements in Javascript/01. Code/02. loop.js
@@ -0,0 +1,5 @@
+// declare/initialize , condition , change 
+for(let i = 1; i<=5 ; i++){
+    console.log("Hello world");
+    
+}
