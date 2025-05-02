85b9207fd79b224c89690a2171843364c9b74a08
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-05-02 18:01:05 +0000

    duplicate zeros

--- 03. DSA/27. Day-63 Advanced Array Questions/Code/03.2. Duplicate_Zeros_Optimal.js
+++ 03. DSA/27. Day-63 Advanced Array Questions/Code/03.2. Duplicate_Zeros_Optimal.js
@@ -1,7 +1,8 @@
 // Duplicate Zeros (leetcode - 1089)  optimal solution
 
 // let arr = [1, 0, 2, 3, 0, 4, 5, 0];
-let arr = [0,0,0,0,0,0,0]
+// let arr = [0,0,0,0,0,0,0]
+let arr = [8,5,0,9,0,3,4,7]
 
 let countZeros = 0;
 
@@ -15,31 +16,21 @@ console.log(countZeros);
 
 let i = arr.length - 1;
 
-let j = 0
-if(countZeros == 0){
-    j = arr.length-1
-}else{
-    j = i + countZeros-1;
-}
+let j = (arr.length-1) + countZeros
 
-while (i >= 0) {
-  if (j < arr.length) {
-    if(arr[i] != 0){
-        arr[j] = arr[i]
-        j-- , i--
-    }else{
-       if(j+1 < arr.length){
-        arr[j] = arr[i]
-        arr[j-1] = arr[i]
-        i-- , j-=2
-       }else{
-        arr[j] = arr[i]
-        i-- , j--
-       }
-    }
-  } else {
-    j--, i--;
+while (i != j) {
+  if(j < arr.length){
+    arr[j] = arr[i]
   }
+  j--
+
+  if(arr[i] == 0){
+    if(j < arr.length){
+      arr[j] = arr[i]
+    }
+    j--
+  }
+  i--
 }
 
 console.log(arr);
