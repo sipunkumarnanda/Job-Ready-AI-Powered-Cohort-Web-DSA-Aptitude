7377231b2e3bf20fbc7c255434643f3477690462
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-06-12 06:58:46 +0000

    add role-based auth, dynamic navbar, and product access control

---
+++ 01. Front-End/59. Day-98 Project-III/frontend/src/store/reducers/productSlice.jsx
@@ -0,0 +1,22 @@
+
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    products : [] , 
+}
+
+const productSlice = createSlice({
+    name : "product",
+    initialState,
+    reducers : {
+        loadproduct : (state, action) =>{
+            // Here we can't call api , its sync
+            state.products = action.payload
+        } , 
+    }
+}
+)
+
+export default productSlice.reducer ;
+export const {loadproduct} =  productSlice.actions ;
