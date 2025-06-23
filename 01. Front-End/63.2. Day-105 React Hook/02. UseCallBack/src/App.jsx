b004c6069702231c721d06f158eb1bd7d6a630af
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-06-23 04:20:05 +0000

    UseEffect , useMemo , Memo, useCallback , Reconcilation , Memoization

--- 01. Front-End/63.2. Day-105 React Hook/02. UseCallBack/src/App.jsx
+++ 01. Front-End/63.2. Day-105 React Hook/02. UseCallBack/src/App.jsx
@@ -5,33 +5,31 @@ const App = () => {
   const [add, setAdd] = useState(0);
   const [sub, setSub] = useState(99);
 
-
   const ingredients = useCallback(() => {
-     console.log("Recipe Ingredients Here");
-  },[sub])   // jab sub me changes ho ta ye func call hoga 
+    console.log("Recipe Ingredients Here");
+  }, [sub]); // jab sub me changes ho ta ye func call hoga
 
   return (
     <div className="py-10 px-10 bg-gray-900 w-screen h-screen text-white font-thin">
       <button
-          onClick={() => setAdd(add + 1)}
-          className="text-2xl font-bold rounded bg-blue-400 hover:bg-blue-500 px-5 py-3 cursor-pointer"
-        >
-          +
-        </button>
-        <span className="text-xl ml-4 font-bold">{add}</span>
-         <br /> <br />
-
-          <button
-           onClick={() => setSub(sub - 1)}
-          className="text-2xl font-bold rounded bg-red-400 hover:bg-red-500 px-5 py-3 cursor-pointer"
-        >
-          -
-        </button>
-        <span className="text-xl ml-5 font-bold">{sub}</span> <br /> <br />
-
-        <Recipe ing={ingredients}/>
+        onClick={() => setAdd(add + 1)}
+        className="text-2xl font-bold rounded bg-blue-400 hover:bg-blue-500 px-5 py-3 cursor-pointer"
+      >
+        +
+      </button>
+      <span className="text-xl ml-4 font-bold">{add}</span>
+      <br /> <br />
+      <button
+        onClick={() => setSub(sub - 1)}
+        className="text-2xl font-bold rounded bg-red-400 hover:bg-red-500 px-5 py-3 cursor-pointer"
+      >
+        -
+      </button>
+      <span className="text-xl ml-5 font-bold">{sub}</span> <br /> <br />
+      <Recipe ing={ingredients} />
     </div>
   );
 };
 
 export default App;
+
