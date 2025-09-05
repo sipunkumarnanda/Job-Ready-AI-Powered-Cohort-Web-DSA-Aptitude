8715c106ec648ff2996f2c02707980e6ede03de8
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-09-05 08:21:27 +0000

    .

---
+++ 04. NextJS/02. Day-163 Nextjs Routing/React/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-[50px] bg-blue-500 flex justify-center items-center absolute bottom-0'>
+        <p>Foodvilla all rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
