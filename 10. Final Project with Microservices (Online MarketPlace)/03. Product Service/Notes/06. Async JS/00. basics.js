cc543603aee6936979f64194e950def1e3ffa9c1
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2026-01-02 09:16:14 +0000

    Added notes on Async JS (Promises) and Multer (Under the hood)

---
+++ 10. Final Project with Microservices (Online MarketPlace)/03. Product Service/Notes/06. Async JS/00. basics.js
@@ -0,0 +1,12 @@
+
+
+async function xyz() {
+    return "Hello"
+}
+
+let arr = [1,2,3,4,5]
+let res = arr.map((val)=> xyz())
+console.log(res);
+// let final = await Promise.resolve(res) // if one promise
+let final = await Promise.all(res) // for multiple promises 
+console.log(final);
