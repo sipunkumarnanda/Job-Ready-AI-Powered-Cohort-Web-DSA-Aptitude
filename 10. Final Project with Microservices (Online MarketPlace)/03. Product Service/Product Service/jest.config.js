27759841d32a3e462106f1d417d8fe4e36a8813b
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2026-01-07 18:27:30 +0000

    add express validator

--- 10. Final Project with Microservices (Online MarketPlace)/03. Product Service/Product Service/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js']
-};
