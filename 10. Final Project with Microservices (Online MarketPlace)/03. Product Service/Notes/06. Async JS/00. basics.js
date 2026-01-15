c014c1fd56517325899951017914472ab2d360c9
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2026-01-15 17:23:56 +0000

    .

--- 10. Final Project with Microservices (Online MarketPlace)/03. Product Service/Notes/06. Async JS/00. basics.js
+++ 10. Final Project with Microservices (Online MarketPlace)/03. Product Service/Notes/06. Async JS/00. basics.js
@@ -5,8 +5,12 @@ async function xyz() {
 }
 
 let arr = [1,2,3,4,5]
+
 let res = arr.map((val)=> xyz())
 console.log(res);
+
 // let final = await Promise.resolve(res) // if one promise
+
 let final = await Promise.all(res) // for multiple promises 
+
 console.log(final);
